476dc9e226b0f15799dd1bbd84197073be936e47
Andrew Badolato <mercenaryelf@gmail.com>
2023-10-18 08:55:55 -0400

    Fix JS

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 //
 // This includes Popper and all of Bootstrap's JS plugins.
 
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js";
 import "../js/simple-scrollspy.min.js";
 
 
